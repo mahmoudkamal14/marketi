70df10a6b58c02da208339ec0fba4dbaebf2e296
Mahmoud Kamal <213179+mahmoudkamalx17@users.noreply.github.com>
2024-10-07 22:57:30 +0300

    انا زهقت يا رجاله

--- lib/features/profile/presentation/screens/personal_info_screen.dart
+++ lib/features/profile/presentation/screens/personal_info_screen.dart
@@ -7,33 +7,16 @@ import 'package:marketi/core/theme/spaces.dart';
 import 'package:marketi/core/widgets/app_text_button.dart';
 import 'package:marketi/core/widgets/app_text_form_field.dart';
 import 'package:marketi/core/widgets/icon_navigate_pop.dart';
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-import 'package:marketi/features/auth/presentation/logic/login/login_cubit.dart';
-=======
-import 'package:marketi/features/auth/data/models/auth_response_model.dart';
-import 'package:marketi/features/profile/presentation/logic/profile_cubit.dart';
-import 'package:marketi/features/profile/presentation/logic/profile_state.dart';
->>>>>>> Stashed changes
-=======
 import 'package:marketi/features/auth/data/models/auth_response_model.dart';
 import 'package:marketi/features/profile/data/models/update_request_body.dart';
 import 'package:marketi/features/profile/presentation/logic/profile_cubit.dart';
 import 'package:marketi/features/profile/presentation/logic/profile_state.dart';
->>>>>>> 80a944cd47ffa868a95724024d072c7894756264
 import 'package:marketi/features/profile/presentation/widgets/image_personal_info.dart';
-import 'package:marketi/features/profile/presentation/widgets/update_user_data_form.dart';
 
-<<<<<<< HEAD
-class PersonalInfoScreen extends StatelessWidget {
-<<<<<<< Updated upstream
-  const PersonalInfoScreen({super.key});
-=======
 class PersonalInfoScreen extends StatefulWidget {
   const PersonalInfoScreen({super.key, required this.userModel});
 
   final AuthResponseModel userModel;
->>>>>>> 80a944cd47ffa868a95724024d072c7894756264
 
   @override
   State<PersonalInfoScreen> createState() => _PersonalInfoScreenState();
@@ -50,65 +33,12 @@ class _PersonalInfoScreenState extends State<PersonalInfoScreen> {
     phoneController.text = widget.userModel.data!.phone!;
     emailController.text = widget.userModel.data!.email!;
 
-<<<<<<< HEAD
-    return Directionality(
-      textDirection: TextDirection.rtl,
-      child: Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 14.w, vertical: 40.h),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    const IconNavigatePop(),
-                    const Spacer(),
-                    Text('المعلومات الشخصية', style: AppStyles.style24Medium),
-                    const Spacer(),
-                  ],
-                ),
-                verticalSpace(30),
-                const ImagePersonalInfo(),
-                verticalSpace(30),
-                Card(
-                  child: SizedBox(
-                    height: 60,
-                    width: double.infinity,
-                    child: ListTile(
-                      leading: const Icon(
-                        Icons.person_3_outlined,
-                        color: Color(0xFF8CB3FF),
-                      ),
-                      title: Text(
-                        user.name!,
-                        style: AppStyles.style20Medium,
-                      ),
-=======
-  const PersonalInfoScreen({super.key, required this.userModel});
-
-  final AuthResponseModel userModel;
-
-  @override
-  Widget build(BuildContext context) {
-=======
     final GlobalKey<FormState> profileFormKey = GlobalKey();
 
->>>>>>> 80a944cd47ffa868a95724024d072c7894756264
     return BlocProvider(
       create: (context) => getIt<ProfileCubit>(),
       child: BlocBuilder<ProfileCubit, ProfileState>(
         builder: (context, state) {
-<<<<<<< HEAD
-          TextEditingController nameController = TextEditingController();
-          TextEditingController phoneController = TextEditingController();
-          TextEditingController emailController = TextEditingController();
-
-          nameController.text = userModel.data!.name!;
-          phoneController.text = userModel.data!.phone!;
-          emailController.text = userModel.data!.email!;
-
-=======
->>>>>>> 80a944cd47ffa868a95724024d072c7894756264
           return Directionality(
             textDirection: TextDirection.rtl,
             child: Scaffold(
@@ -119,11 +49,6 @@ class _PersonalInfoScreenState extends State<PersonalInfoScreen> {
                     vertical: 40.h,
                   ),
                   child: SingleChildScrollView(
-<<<<<<< HEAD
-                    keyboardDismissBehavior:
-                        ScrollViewKeyboardDismissBehavior.manual,
-=======
->>>>>>> 80a944cd47ffa868a95724024d072c7894756264
                     child: Column(
                       children: [
                         Row(
@@ -138,18 +63,6 @@ class _PersonalInfoScreenState extends State<PersonalInfoScreen> {
                           ],
                         ),
                         verticalSpace(30),
-<<<<<<< HEAD
-                        ImagePersonalInfo(userModel: userModel),
-                        verticalSpace(30),
-                        UpdateUserDataForm(
-                          nameController: nameController,
-                          phoneController: phoneController,
-                          emailController: emailController,
-                          userModel: userModel,
-                        ),
-                      ],
->>>>>>> Stashed changes
-=======
                         ImagePersonalInfo(userModel: widget.userModel),
                         verticalSpace(30),
                         Form(
@@ -222,7 +135,6 @@ class _PersonalInfoScreenState extends State<PersonalInfoScreen> {
                           ),
                         ),
                       ],
->>>>>>> 80a944cd47ffa868a95724024d072c7894756264
                     ),
                   ),
                 ),
